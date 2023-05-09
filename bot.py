7751ef9693ed48519e09f3f16279808090a300c0
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-05-09 09:16:21 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+Sure, here's the Python 2 code for the same functionality:
+
+```python
+import urllib2
+import json
+
+# Set your Telegram bot token and channel username
+telegram_token = '5985008411:AAF8uDeTiMKAzDZEOp5UzbnpRKpcbpVCCww'
+channel_username = 'likhon.anon'
+
+# Define the Telegram API endpoint
+telegram_api = 'https://api.telegram.org/bot' + telegram_token + '/sendMessage'
+
+# Get the chat ID and message text from the user's message
+chat_id = message['chat']['id']
+message_text = message['text']
+
+# Check if the user has joined the channel
+url = 'https://api.telegram.org/bot' + telegram_token + '/getChatMember?chat_id=' + channel_username + '&user_id=' + str(chat_id)
+response = urllib2.urlopen(url)
+data = json.load(response)
+is_member = data['result']['status']
+
+# If the user is not a member of the channel, send a message with a join link
+if is_member != 'member' and is_member != 'creator':
+    message = "You must join our channel " + channel_username + " to use this bot. Please join the channel and restart the bot.
+
+Join our channel: https://t.me/" + channel_username + "
+
+Once you have joined the channel, click the /start command to restart the bot."
+    params = {
+        'chat_id': chat_id,
+        'text': message,
+        'parse_mode': 'Markdown',
+    }
+    urllib2.urlopen(telegram_api, urllib.urlencode(params))
+    return
+
+# If the user is a member of the channel, continue with the bot's functionality
+# ...
+
+```
+
+To use this code, replace `YOUR_TELEGRAM_BOT_TOKEN` and `YOUR_TELEGRAM_CHANNEL_USERNAME` with your own values. You can then use this code in your Python 2 Telegram bot to force users to join your channel
