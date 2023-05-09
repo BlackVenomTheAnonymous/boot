7f32d8045b5d00d6af0ac0f4956cd1acc867e33b
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-05-09 09:08:44 +0600

    Update bot.php

--- bot.php
+++ bot.php
@@ -72,3 +72,24 @@ if ($data && isset($data['country'])) {
 
 // Send the message back to the user
 $chat_id = isset
+<?php
+
+// Get the incoming webhook data
+$data = file_get_contents('php://input');
+
+// Parse the JSON data
+$update = json_decode($data, true);
+
+// Get the chat ID and message text from the incoming message
+$chat_id = isset($update['message']['chat']['id']) ? $update['message']['chat']['id'] : '';
+$message_text = isset($update['message']['text']) ? $update['message']['text'] : '';
+
+// Perform the necessary actions based on the message data
+if ($message_text == '/start') {
+    // Send a welcome message to the user
+    $message = "Welcome to my bot!";
+    $params = [
+        'chat_id' => $chat_id,
+        'text' => $message,
+    ];
+    file_get_contents('https://api.telegram.org/bot<your-bot-token>/sendMessage?' . http_build_query($params
